f5f4e3b98d268414aa78d8e68379b629982d9821
chinu-chinmayee <chinu0620@gmail.com>
2025-02-14 00:12:42 +0530

    changed cookies

--- src/functions/yt/downloadVideo.ts
+++ src/functions/yt/downloadVideo.ts
@@ -8,126 +8,6 @@ import uploadMergedVideos from "../../storage/uploadMergedVideo.js";
 const agent = ytdl.createAgent([
   {
     "domain": ".youtube.com",
-    "expirationDate": 1771767247.331227,
-    "hostOnly": false,
-    "httpOnly": true,
-    "name": "__Secure-3PSID",
-    "path": "/",
-    "sameSite": "no_restriction",
-    "secure": true,
-
-    "value": "g.a000sggzMusHYML0oplqO2TovJzivGOax2JoHZvCC0X4mEWgABB9tYkjr8vqqxBJWEjQonWJCAACgYKAYQSAQASFQHGX2MigRjXWEuBD_gxrtfrzcOfTxoVAUF8yKqXL_qQ_A5yCoXdez6YFre20076"
-  },
-  {
-    "domain": ".youtube.com",
-    "expirationDate": 1771002977.292565,
-    "hostOnly": false,
-    "httpOnly": true,
-    "name": "__Secure-1PSIDTS",
-    "path": "/",
-    "sameSite": null,
-    "secure": true,
-
-    "value": "sidts-CjIBEJ3XVyZCL0zo6cx5agXPCWAnHCoYXETcbduIowWE3pDbon0-aku3kJ5xvj0oFYTfIBAA"
-  },
-  {
-    "domain": ".youtube.com",
-    "expirationDate": 1771767247.330953,
-    "hostOnly": false,
-    "httpOnly": false,
-    "name": "SAPISID",
-    "path": "/",
-    "sameSite": null,
-    "secure": true,
-
-    "value": "w30EnxT87uAuRI3r/AkrmMxiPqzSTDJHdS"
-  },
-  {
-    "domain": ".youtube.com",
-    "expirationDate": 1771002987.793457,
-    "hostOnly": false,
-    "httpOnly": true,
-    "name": "__Secure-1PSIDCC",
-    "path": "/",
-    "sameSite": null,
-    "secure": true,
-
-    "value": "AKEyXzVZkaggpaGSNRG0xyLZgIlLZdOP8ih6z-ZU2ma8hyh-ORoPpIPxYhygF-aBVJzSV9yMSZk"
-  },
-  {
-    "domain": ".youtube.com",
-    "expirationDate": 1771767247.330914,
-    "hostOnly": false,
-    "httpOnly": true,
-    "name": "SSID",
-    "path": "/",
-    "sameSite": null,
-    "secure": true,
-
-    "value": "AbC4r2VHddb99x84Y"
-  },
-  {
-    "domain": ".youtube.com",
-    "expirationDate": 1771767247.33097,
-    "hostOnly": false,
-    "httpOnly": false,
-    "name": "__Secure-1PAPISID",
-    "path": "/",
-    "sameSite": null,
-    "secure": true,
-
-    "value": "w30EnxT87uAuRI3r/AkrmMxiPqzSTDJHdS"
-  },
-  {
-    "domain": ".youtube.com",
-    "expirationDate": 1771767247.331211,
-    "hostOnly": false,
-    "httpOnly": true,
-    "name": "__Secure-1PSID",
-    "path": "/",
-    "sameSite": null,
-    "secure": true,
-
-    "value": "g.a000sggzMusHYML0oplqO2TovJzivGOax2JoHZvCC0X4mEWgABB9NRs4zVsn1TS5Y5QsX3yajAACgYKAYASAQASFQHGX2MiWwzb-WLkRa2BnbCUMRmaahoVAUF8yKoeb026Xjsqgb-mj5zg5ZPO0076"
-  },
-  {
-    "domain": ".youtube.com",
-    "expirationDate": 1771767247.330984,
-    "hostOnly": false,
-    "httpOnly": false,
-    "name": "__Secure-3PAPISID",
-    "path": "/",
-    "sameSite": "no_restriction",
-    "secure": true,
-
-    "value": "w30EnxT87uAuRI3r/AkrmMxiPqzSTDJHdS"
-  },
-  {
-    "domain": ".youtube.com",
-    "expirationDate": 1771002987.79357,
-    "hostOnly": false,
-    "httpOnly": true,
-    "name": "__Secure-3PSIDCC",
-    "path": "/",
-    "sameSite": "no_restriction",
-    "secure": true,
-
-    "value": "AKEyXzWiK_x-ySXEzRgnn0gKpKBzVJul7sxUIqnE8HTd5YTPNgzfrHQ6UtY83VmCMQwP6gUUrqQ"
-  },
-  {
-    "domain": ".youtube.com",
-    "expirationDate": 1771002977.292753,
-    "hostOnly": false,
-    "httpOnly": true,
-    "name": "__Secure-3PSIDTS",
-    "path": "/",
-    "sameSite": "no_restriction",
-    "secure": true,
-
-    "value": "sidts-CjIBEJ3XVyZCL0zo6cx5agXPCWAnHCoYXETcbduIowWE3pDbon0-aku3kJ5xvj0oFYTfIBAA"
-  },
-  {
-    "domain": ".youtube.com",
     "expirationDate": 1756910310.327552,
     "hostOnly": false,
     "httpOnly": true,
@@ -135,20 +15,7 @@ const agent = ytdl.createAgent([
     "path": "/",
     "sameSite": "no_restriction",
     "secure": true,
-
     "value": "AFmmF2swRQIgT5Td6jEL_MSSjb5TLuZACIndfHxZTDIbTud7wXvg3s0CIQDqyDc6xSdl7vKbscKusjtdP2wJMHTALPvHmPFBAdIqgA:QUQ3MjNmeUg0Z0RiaEZVSk0zRHBtR0haOE9Nbnc0UU15RUt4eUc1NTJfZEozNUZZN0ZNVlVxODlaMDloaENoZVZDYW5mWkd1S3VmUDF6eUo2U01QMHFfN0d3NERLNDRpUU04NUhWMlZkZVU2Uk5qVTlaX3hrSF9DUmNkLTdlVlZQaE9ON1ptZHFXSXFQTEhEWG9Kb2VvbWlmZ3U0a2N5NTln"
-  },
-  {
-    "domain": ".youtube.com",
-    "expirationDate": 1774026975.50194,
-    "hostOnly": false,
-    "httpOnly": false,
-    "name": "PREF",
-    "path": "/",
-    "sameSite": null,
-    "secure": true,
-
-    "value": "f6=40000000&f7=4100&tz=Asia.Calcutta&f4=4000000&f5=30000"
   }
 ]);
 
@@ -178,7 +45,7 @@ export default async function DownloadVideo(
 
   try {
     // return false;
-    const info = await ytdl.getInfo(link,{agent});
+    const info = await ytdl.getInfo(link, { agent });
     console.log("Info Collected")
     const formats = info.formats;
 
@@ -225,6 +92,8 @@ export default async function DownloadVideo(
             return format.qualityLabel === quality
           }
         },
+        agent:agent
+       
       },)
         .on("progress", async (chunkLength, downloaded, total) => {
 
@@ -263,6 +132,7 @@ export default async function DownloadVideo(
       let percent = 0;
       ytdl(link, {
         filter: (format) => format.audioCodec === audioFormat.audioCodec,
+        agent
       })
         .on("progress", async (chunkLength, downloaded, total) => {
           const currentPercent = (downloaded / total) * 100;
